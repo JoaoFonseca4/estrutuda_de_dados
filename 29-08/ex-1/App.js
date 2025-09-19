f83965268a17cea69f6156a352692d12ff9d2755
João Francisco Fabbri Fonseca <joao.fonseca27@fatec.sp.gov.br>
2025-09-18 21:43:28 -0300

    exercícios

--- 29-08/ex-1/App.js
+++ 29-08/ex-1/App.js
@@ -20,21 +20,6 @@ console.log(empresa.itens)
 
 
 
-const empresa2 = new MeuArray
-
-empresa2.adicionar("João")
-empresa2.adicionar("Pedro")
-empresa2.adicionar("Leticia")
-empresa2.adicionar("Andre")
-
-console.log(empresa2.obterElemento(2))
-
-empresa2.limpar()
-
-console.log(empresa2.itens)
-
-console.log(empresa2.obterElemento(2))
-
 
 
 
